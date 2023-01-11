9c10eefc68dfa599257923892d9844d480104712
= <=>
2023-01-11 15:38:47 -0300

    post

--- src/config/database.js
+++ src/config/database.js
@@ -2,19 +2,20 @@ require('dotenv').config();
 
 module.exports = {
   /* SQLite */
-  dialect: 'sqlite',
-  storage: './db.sqlite',
+  // dialect: 'sqlite',
+  // storage: "db.sqlite",
 
   /* MySQL / MariaDB */
-  // host: process.env.DATABASE_HOST,
-  // port: process.env.DATABASE_PORT,
-  // username: process.env.DATABASE_USERNAME,
-  // password: process.env.DATABASE_PASSWORD,
-  // database: process.env.DATABASE,
-  // dialectOptions: {
-  //   timezone: 'America/Sao_Paulo',
-  // },
-  // timezone: 'America/Sao_Paulo',
+  dialect: 'mariadb',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  dialectOptions: {
+    timezone: 'America/Sao_Paulo',
+  },
+  timezone: 'America/Sao_Paulo',
 
   /* ALL */
   define: {
