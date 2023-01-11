e804fc0719d665afa6d505991edf92226f947890
= <=>
2023-01-11 16:29:53 -0300

    post

--- src/config/database.js
+++ src/config/database.js
@@ -1,28 +1,28 @@
 require('dotenv').config();
 
 module.exports = {
-  /* SQLite */
-  // dialect: 'sqlite',
-  // storage: "db.sqlite",
+    /* SQLite */
+    // dialect: 'sqlite',
+    // storage: "db.sqlite",
 
-  /* MySQL / MariaDB */
-  dialect: 'mariadb',
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-  dialectOptions: {
+    /* MySQL / MariaDB */
+    dialect: 'mariadb',
+    host: process.env.DATABASE_HOST || "192.168.25.152",
+    port: process.env.DATABASE_PORT || "3306",
+    username: process.env.DATABASE_USERNAME || "root",
+    password: process.env.DATABASE_PASSWORD || "Limber301020",
+    database: process.env.DATABASE || "escola",
+    dialectOptions: {
+        timezone: 'America/Sao_Paulo',
+    },
     timezone: 'America/Sao_Paulo',
-  },
-  timezone: 'America/Sao_Paulo',
 
-  /* ALL */
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-  },
+    /* ALL */
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    },
 };
