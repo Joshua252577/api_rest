8ab8d99709f6f0b28fe01515218e3701bfdebc6a
= <=>
2023-01-11 14:39:35 -0300

    post

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import app from './app';
 
-const port = process.env.APP_PORT;
-app.listen(port);
+const port = process.env.APP_PORT ||5555;
+app.listen(port, ()=>{
+    console.log(`${port}`);
+});
