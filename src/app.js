aa4a720fb5e14a099a6870dd477fcf1f75a178af
= <=>
2023-01-13 15:02:27 -0300

    alterações na Api

--- src/app.js
+++ src/app.js
@@ -37,9 +37,7 @@ class App {
     }
 
     middlewares() {
-        this.app.use(
-            '*', cors()
-        );
+        this.app.use(cors(corsOptions));
         this.app.use(helmet());
         this.app.use(express.urlencoded({extended: true}));
         this.app.use(express.json());
